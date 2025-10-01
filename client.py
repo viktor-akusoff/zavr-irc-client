5af6d0f955368ff917322a5d54406d64cb476c0c
viktor-akusoff <kirill.voloshin.98@ya.ru>
2025-10-01 12:33:50 +0300

    implementing sender task and client as context manager

--- client.py
+++ client.py
@@ -19,6 +19,7 @@ class ClientServiceMessages(str, enum.Enum):
 
     CONNECTION_ERROR = "connection_error"
     RECEIVER_STOPPED = "receiver_stopped"
+    SENDER_STOPPED = "sender_stopped"
     SERVER_CLOSED_CONNECTION = "server_closed_connection"
     MALFUNCTIONING_SERVER = "malfunctioning_server"
     UNKNOWN_ERROR = "unknown_error"
@@ -30,8 +31,29 @@ class ClientConfig:
 
     host: str
     port: int
-    tls: bool
-    errors: Optional[Literal["ignore", "replace"]]
+    tls: bool = False
+    encoding: str = "utf-8"
+    errors: Optional[Literal["ignore", "replace"]] = "ignore"
+
+
+@dataclasses.dataclass
+class ClientQueues:
+    """Stores three client queues used for sending, receiving messages
+    and controlling client status.
+    """
+
+    receive: queue.Queue[str] = queue.Queue()
+    send: queue.Queue[str] = queue.Queue()
+    status: queue.Queue[ClientServiceMessages] = queue.Queue()
+
+
+@dataclasses.dataclass
+class ClientState:
+    """Stores current client's state."""
+
+    is_running: bool = False
+    stop_event: threading.Event = dataclasses.field(default_factory=threading.Event)
+    lock: threading.Lock = dataclasses.field(default_factory=threading.Lock)
 
 
 class ZavrClient:
@@ -53,12 +75,11 @@ class ZavrClient:
         """
 
         self._config = config
-
-        self._stop_event = threading.Event()
+        self._state = ClientState()
         self._socket: Optional[socket.SocketType] = None
-
-        self.receive_queue: queue.Queue[str | ClientServiceMessages] = queue.Queue()
-        self.send_queue: queue.Queue[str] = queue.Queue()
+        self._receiver_thread: threading.Thread | None = None
+        self._sender_thread: threading.Thread | None = None
+        self._queues: ClientQueues = ClientQueues()
 
     def _connect(self):
         """Connects to an IRC server given at initialization."""
@@ -86,7 +107,7 @@ class ZavrClient:
         buffer = bytearray()
 
         try:
-            while not self._stop_event.is_set():
+            while not self._state.stop_event.is_set():
 
                 try:
                     data = self._socket.recv(self.BUFFER_SIZE)
@@ -98,7 +119,7 @@ class ZavrClient:
                         self._config.host,
                         self._config.port,
                     )
-                    self.receive_queue.put(ClientServiceMessages.CONNECTION_ERROR)
+                    self._queues.status.put(ClientServiceMessages.CONNECTION_ERROR)
                     break
                 except Exception:  # pylint: disable=W0718
                     logger.exception(
@@ -106,7 +127,7 @@ class ZavrClient:
                         self._config.host,
                         self._config.port,
                     )
-                    self.receive_queue.put(ClientServiceMessages.UNKNOWN_ERROR)
+                    self._queues.status.put(ClientServiceMessages.UNKNOWN_ERROR)
                     break
 
                 if not data:
@@ -115,7 +136,7 @@ class ZavrClient:
                         self._config.host,
                         self._config.port,
                     )
-                    self.receive_queue.put(
+                    self._queues.status.put(
                         ClientServiceMessages.SERVER_CLOSED_CONNECTION
                     )
                     break
@@ -126,7 +147,7 @@ class ZavrClient:
                         self._config.host,
                         self._config.port,
                     )
-                    self.receive_queue.put(ClientServiceMessages.MALFUNCTIONING_SERVER)
+                    self._queues.status.put(ClientServiceMessages.MALFUNCTIONING_SERVER)
                     break
 
                 buffer.extend(data)
@@ -135,21 +156,148 @@ class ZavrClient:
                     message = buffer[:position]
                     del buffer[: position + 2]
 
-                    self.receive_queue.put(
-                        message.decode("utf-8", errors=self._config.errors)
+                    self._queues.receive.put(
+                        message.decode(
+                            self._config.encoding, errors=self._config.errors
+                        )
                     )
         finally:
-            self.receive_queue.put(ClientServiceMessages.RECEIVER_STOPPED)
+            if not self._state.stop_event.is_set():
+                self._state.stop_event.set()
+            self._queues.status.put(ClientServiceMessages.RECEIVER_STOPPED)
             logger.info(
                 "Receiver task for %s:%d has stopped.",
                 self._config.host,
                 self._config.port,
             )
 
-    def _send_task(self): ...
+    def _send_task(self):
 
-    def start(self): ...
+        if self._socket is None:
+            raise exceptions.ClientIsNotConnectedError("Client is not connected!")
+
+        try:
+            while not self._state.stop_event.is_set():
+                try:
+                    message_to_send = self._queues.send.get(timeout=1.0)
+                except queue.Empty:
+                    continue
+                encoded_message = (message_to_send + "\r\n").encode(
+                    self._config.encoding, errors="replace"
+                )
+
+                if len(encoded_message) > 512:
+                    logger.warning(
+                        "Attempted to send message longer than 512 bytes to %s:%d. Skipping.",
+                        self._config.host,
+                        self._config.port,
+                    )
+                    continue
+                try:
+                    self._socket.sendall(encoded_message)
+                except (socket.error, OSError, ssl.SSLError) as e:
+                    logger.error(
+                        "Socket error during send on %s:%d: %s",
+                        self._config.host,
+                        self._config.port,
+                        e,
+                    )
+                    self._queues.status.put(ClientServiceMessages.CONNECTION_ERROR)
+                    break
+                except Exception:  # pylint: disable=W0718
+                    logger.exception(
+                        "Unknown error occurred in sender task for %s:%d!",
+                        self._config.host,
+                        self._config.port,
+                    )
+                    self._queues.status.put(ClientServiceMessages.UNKNOWN_ERROR)
+                    break
+        finally:
+            if not self._state.stop_event.is_set():
+                self._state.stop_event.set()
+            logger.info(
+                "Sender task for %s:%d has stopped.",
+                self._config.host,
+                self._config.port,
+            )
+            self._queues.status.put(ClientServiceMessages.SENDER_STOPPED)
+
+    def start(self):
+        "Starts sender and receiver threads for client."
+
+        with self._state.lock:
+            if self._state.is_running:
+                return
+            self._state.is_running = True
+
+        self._queues = ClientQueues()
+        self._state.stop_event = threading.Event()
+
+        try:
+            self._connect()
+        except exceptions.ClientConnectionError:
+            logger.error(
+                "Failed to connect to %s:%d", self._config.host, self._config.port
+            )
+            self._queues.status.put(ClientServiceMessages.CONNECTION_ERROR)
+            return
+
+        self._sender_thread = threading.Thread(target=self._send_task, daemon=True)
+        self._receiver_thread = threading.Thread(target=self._receive_task, daemon=True)
+
+        self._sender_thread.start()
+        self._receiver_thread.start()
 
     def stop(self):
         """Stops client and its threads receiving and sending threads."""
-        self._stop_event.set()
+
+        with self._state.lock:
+            if not self._state.is_running:
+                return
+            self._state.is_running = False
+
+        self._state.stop_event.set()
+
+        if self._sender_thread is not None:
+            self._sender_thread.join()
+
+        if self._receiver_thread is not None:
+            self._receiver_thread.join()
+
+        if self._socket is not None:
+            self._socket.close()
+            self._socket = None
+
+        self._sender_thread = None
+        self._receiver_thread = None
+
+    def get_message(self) -> str | None:
+        """Returns received message from queue"""
+
+        try:
+            return self._queues.receive.get(block=False)
+        except queue.Empty:
+            return None
+
+    def get_status_message(self) -> ClientServiceMessages | None:
+        """Returns received message from queue"""
+
+        try:
+            return self._queues.status.get(block=False)
+        except queue.Empty:
+            return None
+
+    def send_message(self, message: str):
+        """Sends messages to server.
+
+        Args:
+            message (str): Message to send to server.
+        """
+        self._queues.send.put(message)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
